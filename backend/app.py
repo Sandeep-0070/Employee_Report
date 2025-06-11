6f76a4596ca4fd2346751c971f1f9d95b2fbb99d
Siva Sandeep S <sandeepsivakanthan@gmail.com>
2025-06-11 17:21:40 +0530

    Update app.py to work with Render deployment

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ import io
 from flask import send_file
 import csv
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -143,4 +144,5 @@ def export_excel():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
